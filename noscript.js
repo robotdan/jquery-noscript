997fa9c27da89d57fa6dbf0b832717b2741cb721
Daniel DeGroff <djdegroff@gmail.com>
2014-02-20 00:26:38 -0600

    Cleanup, starting no script.css

--- noscript.js
+++ noscript.js
@@ -1,36 +1,43 @@
-
 /**
+ * 
+ * jQuery Noscript v0.1
+ *
  * Provide some automatic handling of form controls, enable and disable elments based upon other values.
  *
  * Example:
- *   Cause the input text field named 'text' to be enbled when 'foo' is selected, when 'bar' is selected
- *     the input field will be disabled. The values to these two data- attributes are selectors. The more
- *     generic the selector, the greater possibility for there to be ambiguity in matching the originating
- *     event. It is up to the caller to make the value selector specific enough to remove ambiguity.
+ *	 Cause the input text field named 'text' to be enbled when 'foo' is selected, when 'bar' is selected
+ *	   the input field will be disabled. The values to these two data- attributes are selectors. The more
+ *	   generic the selector, the greater possibility for there to be ambiguity in matching the originating
+ *	   event. It is up to the caller to make the value selector specific enough to remove ambiguity.
  *
- *   <select name="objectType">
- *   	<option value="foo">foo</option>
- *      <option value="bar">bar</option>
- *   </select>
+ *	 <select name="objectType">
+ *		<option value="foo">foo</option>
+ *		<option value="bar">bar</option>
+ *	 </select>
  *
- *   <input name="text" type="text" data-enable-on-trigger="[name=objectType]" data-value="[value=foo]:selected" >
+ *	 <input name="text" type="text" data-ns-enable="[name=objectType]" data-ns-value="[value=foo]" >
+ *
+ * Released under the MIT license
+ *
  */
 (function ( $ ) {
+	
+	"use strict";
 
 	$.fn.noscript = function() {
 
 		// identify controllers by collecting the selectors from the listeners
 		var container = this.is( "form" ) ? this : this.find( "form" ),
 			controllers = {},
-			triggerTypes = [ "[data-enable-on-trigger]", "[data-show-on-trigger]" ],
+			triggerTypes = [ "[data-ns-enable]", "[data-ns-show]" ],
 			toCamelCase = function(string) {
-				// [data-some-name] --> someName
+				// [data-ns-show] --> nsShow
 				return string.substring(6, string.length - 1).replace(/([ -]+)([a-zA-Z0-9])/g, function(a,b,c) {
 					return c.toUpperCase();
 				}).replace(/-/g, "");
 			};
 
-	    $.each( triggerTypes, function( i, dataSelector ) {
+		$.each( triggerTypes, function( i, dataSelector ) {
 
 			container.find( dataSelector ).each( function() {
 
@@ -46,14 +53,14 @@
 			});
 		});
 
-	    // bind change event on all select, checkbox and radio controls
+		// bind change event on all select, checkbox and radio controls
 		container.on( "change", "select, input[type=checkbox], input[type=radio]", function( event ) {
 
 			var originator = $( event.currentTarget ),
 				changedListeners = [];
 
 			// handle enable/disable
-			container.find( "[data-enable-on-trigger]" ).each( function() {
+			container.find( "[data-ns-enable]" ).each( function() {
 
 				$( this ).each( function() {
 				
@@ -61,10 +68,10 @@
 						data = listener.data();
 
 					// if listener is interested in this controller
-					if ( originator.is( data.enableOnTrigger ) ) {
+					if ( originator.is( data.nsEnable ) ) {
 
 						// Default listener to disabled.
-						var selector = data.value,
+						var selector = data.nsValue,
 							disable = true;
 
 						// If the originator is disabled, the listener should be as well.
@@ -77,39 +84,39 @@
 							}
 						}
 
-	                    var addToChangedListeners = true;
-	                    if ( listener.is( "tr, td" ) ) {
-	                        listener.toggleClass( "vs-disabled", disable );
-	                    } else {
-	                        // Don't fire a change event for an unchecked radio button
-	                        addToChangedListeners = !listener.is( "[type=radio]" ) || listener.prop( "checked" );
-	                        listener.prop( "disabled", disable );
-	                    }
+						var addToChangedListeners = true;
+						if ( listener.is( "tr, td" ) ) {
+							listener.toggleClass( "ns-disabled", disable );
+						} else {
+							// Don't fire a change event for an unchecked radio button
+							addToChangedListeners = !listener.is( "[type=radio]" ) || listener.prop( "checked" );
+							listener.prop( "disabled", disable );
+						}
 
-	                    if ( addToChangedListeners ) {
-	                        // if this listener is also a controller, keep track so we can trigger a change event
-	                        $.each( controllers, function( selector ) {
-	                            if ( listener.is( selector )) {
-	                                changedListeners.push( listener );
-	                                return false; // break
-	                            }
-	                        });
-	                    }
-	                }
-	            });
-	        });
+						if ( addToChangedListeners ) {
+							// if this listener is also a controller, keep track so we can trigger a change event
+							$.each( controllers, function( selector ) {
+								if ( listener.is( selector )) {
+									changedListeners.push( listener );
+									return false; // break
+								}
+							});
+						}
+					}
+				});
+			});
 
 			// handle show/hide
-			container.find( "[data-show-on-trigger]" ).each( function() {
+			container.find( "[data-ns-show]" ).each( function() {
 			
 				var listener = $( this ),
 					data = listener.data();
 
-				if ( originator.is( data.showOnTrigger ) ) {
+				if ( originator.is( data.nsShow ) ) {
 
-					var selector = data.value,
+					var selector = data.nsValue,
 						hide = true;
-				
+
 					// If the originator is disabled, the listener should be hidden regardless of the value
 					if ( !originator.prop( "disabled" ) ) {
 						if ( originator.is( "select" ) ) {
@@ -135,22 +142,22 @@
 
 		});
 
-	    // Fire controllers on init to set initial state
-	    $.each( controllers, function( selector ) {
-	        // further iterate on these, a selector for a radio may have more than one match.
-	        container.find( selector ).each( function() {
-	            var t = $( this );
-	            if ( !t.prop( "disabled" ) ) {
-	                if ( t.is( "[type=radio]" ) ) {
-	                    if ( t.is( ":checked" ) ) {
-	                        t.change();
-	                    }
-	                } else {
-	                    t.change();
-	                }
-	            }
-	        });
-	    });
+		// Fire controllers on init to set initial state
+		$.each( controllers, function( selector ) {
+			// further iterate on these, a selector for a radio may have more than one match.
+			container.find( selector ).each( function() {
+				var t = $( this );
+				if ( !t.prop( "disabled" ) ) {
+					if ( t.is( "[type=radio]" ) ) {
+						if ( t.is( ":checked" ) ) {
+							t.change();
+						}
+					} else {
+						t.change();
+					}
+				}
+			});
+		});
 	};
 
 }( jQuery ));
@@ -161,4 +168,3 @@
 $( document ).ready( function() {
 	$( "body" ).find( "form[noscript]" ).noscript();
 });
-	
